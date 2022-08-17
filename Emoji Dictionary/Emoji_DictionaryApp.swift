b296bb170ecb9c956d0d1d7c25b5de221b9f60ea
TamiTomi <103371814+TamiTomi@users.noreply.github.com>
2022-08-17 22:50:52 +0300

    Initial Commit

---
+++ Emoji Dictionary/Emoji_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_DictionaryApp.swift
+//  Emoji Dictionary
+//
+//  Created by Toni Tani on 17.8.2022.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
